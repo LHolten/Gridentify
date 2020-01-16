d7faccabdcc6be11c87c204dd443fe00924963e4
Hytak <lhc.holten@gmail.com>
2020-01-16 10:19:48 +0100

    Remove a few messages

--- src/connection.rs
+++ src/connection.rs
@@ -1,45 +1,37 @@
 use serde::{Deserialize, Serialize};
-use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Result, Write};
 use std::net::TcpStream;
 use tungstenite::{Message, WebSocket};
 
-pub trait JsonConnection {
-    fn send<T: Serialize>(&mut self, data: &T) -> Result<(), Error>;
-    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T, Error>;
-    fn set_nodelay(&mut self, v: bool) -> Result<(), Error>;
+pub(crate) trait JsonConnection {
+    fn send<T: Serialize>(&mut self, data: &T) -> Result<()>;
+    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T>;
+    fn set_nodelay(&mut self, v: bool) -> Result<()>;
 }
 
 impl JsonConnection for TcpStream {
-    fn send<T: Serialize>(&mut self, data: &T) -> Result<(), Error> {
+    fn send<T: Serialize>(&mut self, data: &T) -> Result<()> {
         let mut vec_data = serde_json::to_vec(data).unwrap();
         vec_data.push(0);
         self.write(vec_data.as_slice())?;
         Ok(())
     }
 
-    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T, Error> {
+    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T> {
         let mut data = Vec::new();
-        let num_bytes = BufReader::new(self).read_until(0, &mut data)?;
-        if num_bytes == 0 {
-            return Err(Error::new(
-                ErrorKind::InvalidData,
-                "couldn\'t deserialize data",
-            ));
-        }
-        data.pop().unwrap();
-        serde_json::from_slice(data.as_slice()).or(Err(Error::new(
-            ErrorKind::InvalidData,
-            "couldn\'t deserialize data",
-        )))
+        BufReader::new(self).read_until(0, &mut data)?;
+        data.pop()
+            .ok_or(Error::new(ErrorKind::InvalidData, "no message"))?;
+        Ok(serde_json::from_slice(data.as_slice())?)
     }
 
-    fn set_nodelay(&mut self, v: bool) -> Result<(), Error> {
+    fn set_nodelay(&mut self, v: bool) -> Result<()> {
         TcpStream::set_nodelay(&self, v)
     }
 }
 
 impl JsonConnection for WebSocket<TcpStream> {
-    fn send<T: Serialize>(&mut self, data: &T) -> Result<(), Error> {
+    fn send<T: Serialize>(&mut self, data: &T) -> Result<()> {
         let string_data = serde_json::to_string(data).unwrap();
         self.write_message(Message::Text(string_data))
             .or(Err(Error::new(
@@ -53,21 +45,18 @@ impl JsonConnection for WebSocket<TcpStream> {
         Ok(())
     }
 
-    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T, Error> {
+    fn receive<T: for<'a> Deserialize<'a>>(&mut self) -> Result<T> {
         let message = self.read_message().or(Err(Error::new(
             ErrorKind::Interrupted,
             "couldn\'t read message",
         )))?;
         if let Message::Text(value) = message {
-            return serde_json::from_str(value.as_str()).or(Err(Error::new(
-                ErrorKind::InvalidData,
-                "couldn\'t deserialize data",
-            )));
+            return Ok(serde_json::from_str(value.as_str())?);
         }
         Err(Error::new(ErrorKind::InvalidData, "got weird message"))
     }
 
-    fn set_nodelay(&mut self, v: bool) -> Result<(), Error> {
+    fn set_nodelay(&mut self, v: bool) -> Result<()> {
         self.get_mut().set_nodelay(v)
     }
 }
